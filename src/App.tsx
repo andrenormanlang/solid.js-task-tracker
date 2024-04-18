c5c5ee6606fadba6a5e550a856dc2ad9daea01b3
André Norman Lang <andrenormanlang@gmail.com>
2024-04-18 08:55:19 +0200

    constructed visual building blocks with bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,44 @@ import styles from './App.module.css';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class="container mt-5 text-center">
+      <h1>Task Tracker</h1>
+      <form class="mb-5 row row-cols-2 justify-content-center">
+        <input 
+          type="text" 
+          placeholder="Add Task" 
+          class="input-group-text p-1 w-25"
+          id="taskInput"
+          required
+        />
+        <button class="btn btn-primary ms-3 w-auto" type="submit">
+          Add Task
+        </button>
+      </form>
+      <div>
+        <div class="row row-cols-3 mb-3 justify-content-center">
+          <button class="btn btn-secondary w-auto">X</button>
+          <div class="bg-light p-2 mx-2">Learn Solid.js main principles</div>
+          <input 
+            type="checkbox"
+            role='button'
+            class="form-check-input h-auto px-3" 
+          />
+        </div>
+        <div class="row row-cols-3 mb-3 justify-content-center">
+          <button class="btn btn-secondary w-auto">X</button>
+          <div class="bg-light p-2 mx-2 text-decoration-line-through">
+            Update photo APP Rest API
+          </div>
+          <input 
+            type="checkbox"
+            checked
+            role='button'
+            class="form-check-input h-auto px-3" 
+          />
+
+        </div>
+      </div>    
     </div>
   );
 };
